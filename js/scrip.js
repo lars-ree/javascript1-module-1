d0bc11eb07ff88732d35acb8692c27e0451dd372
lars-ree <70200269+lars-ree@users.noreply.github.com>
2021-01-18 12:10:16 +0100

    s

---
+++ js/scrip.js
@@ -0,0 +1,31 @@
+// Question 1
+
+const instrument = "guitar";
+
+console.log(instrument);
+
+// Question 2
+
+const button = document.querySelector("button.login");
+
+console.log(button);
+
+
+// Question 3
+
+const instruments = [
+    {
+        type: "guitar",
+        colour: "red",
+
+    },
+    {
+        type: "piano",
+        colour: "black",
+    },
+];
+
+console.log(instruments);
+
+
+// Question 4
